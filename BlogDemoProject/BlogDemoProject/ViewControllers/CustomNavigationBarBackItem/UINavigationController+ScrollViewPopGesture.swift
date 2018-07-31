6a7de7ac4a33796d5f6d0498645dea1f8a8649a8
qinyuelong <qinyuelong.cn@gmail.com>
2018-07-31 16:24:45 +0800

    添加导航控制器的 优雅地在Swift4中实现Method Swizzling

--- BlogDemoProject/BlogDemoProject/ViewControllers/CustomNavigationBarBackItem/UINavigationController+ScrollViewPopGesture.swift
+++ BlogDemoProject/BlogDemoProject/ViewControllers/CustomNavigationBarBackItem/UINavigationController+ScrollViewPopGesture.swift
@@ -10,19 +10,82 @@
 import Foundation
 import UIKit
 
-extension UINavigationController{
-    // 实现 dispatch_once 
-    private static let scrollViewPopGesture: Void = {
-        
-    }()
+
+protocol SelfAware: class{
+    static func awake()
+}
+
+
+extension UINavigationController: SelfAware{
     
-    static func swapScrollViewPopGesture(){
-        scrollViewPopGesture
+    static func awake() {
+        UINavigationController.classInit()
     }
     
-}
-
-
-extension UINavigationController{
+    static func classInit(){
+        swizzleMethod
+    }
     
+    
+    @objc func swizzled_viewWillAppear(_ animated: Bool) {
+        swizzled_viewWillAppear(animated)
+        print("-------- swizzled_viewWillAppear ----------")
+    }
+    
+    
+    
+    private static let swizzleMethod: Void = {
+        let originalSelector = #selector(viewWillAppear(_:))
+        let swizzledSelector = #selector(swizzled_viewWillAppear(_:))
+        
+        let originalMethod = class_getInstanceMethod(UINavigationController.self, originalSelector)
+        let swizzledMethod = class_getInstanceMethod(UINavigationController.self, swizzledSelector)
+        
+        guard(originalMethod != nil && swizzledMethod != nil) else {
+            return
+        }
+        
+        let didAddMethod = class_addMethod(UINavigationController.self,
+                                           originalSelector,
+                                           method_getImplementation(swizzledMethod!),
+                                           method_getTypeEncoding(swizzledMethod!))
+        if(didAddMethod){
+            //
+            class_replaceMethod(UINavigationController.self,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod!),
+                                method_getTypeEncoding(originalMethod!))
+        }else{
+            method_exchangeImplementations(originalMethod!, swizzledMethod!)
+        }
+    }()
 }
+
+// 遍历调用 awake 的初始化 替换方法 (如果只有单个 可单独调用)
+class NothingToSeeHere {
+    static func harmlessFunction() {
+        let typeCount = Int(objc_getClassList(nil, 0))
+        let types = UnsafeMutablePointer<AnyClass>.allocate(capacity: typeCount)
+        let autoreleasingTypes = AutoreleasingUnsafeMutablePointer<AnyClass>(types)
+        objc_getClassList(autoreleasingTypes, Int32(typeCount))
+        for index in 0 ..< typeCount {
+            (types[index] as? SelfAware.Type)?.awake()
+        }
+        types.deallocate()
+    }
+}
+
+
+// 实现 load 方法 调用替换方法
+extension UIApplication {
+    private static let runOnce: Void = {
+//        NothingToSeeHere.harmlessFunction()
+        UINavigationController.awake()
+    }()
+    
+    override open var next: UIResponder? {
+        UIApplication.runOnce
+        return super.next
+    }
+}
+

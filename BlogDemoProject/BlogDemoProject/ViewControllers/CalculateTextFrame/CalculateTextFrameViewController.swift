236592aaed50bf2764c714fa6a3373931f7fda15
qinyuelong <qinyuelong.cn@gmail.com>
2018-11-08 16:02:09 +0800

    添加博客截图文件

--- BlogDemoProject/BlogDemoProject/ViewControllers/CalculateTextFrame/CalculateTextFrameViewController.swift
+++ BlogDemoProject/BlogDemoProject/ViewControllers/CalculateTextFrame/CalculateTextFrameViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class CalculateTextFrameViewController: BaseViewController {
 
-    @IBOutlet weak var label: UIView!
+    @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        _ = calculateTextFrame(text: "", limitSize: .zero)
     }
     
 
     
     @IBAction func changeTextAction(_ sender: UIButton) {
-        
+        label.text = "line1\nline2"
+    }
+    
+    func calculateTextFrame(text: NSString, limitSize: CGSize) -> CGSize {
+        let rect = text.boundingRect(with: limitSize, options: .usesLineFragmentOrigin, attributes: nil, context: nil)
+        return rect.size
     }
 }
